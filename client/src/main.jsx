07811dfa493565502e6d3c53f86a7f94ade6ff14
Saismiruthi <smiruthisai22@gmail.com>
2026-07-05 12:11:41 +0530

    first commit

---
+++ client/src/main.jsx
@@ -0,0 +1,32 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import { AppContextProvider } from './context/AppContext.jsx'
+import { BrowserRouter } from 'react-router-dom'
+import {ClerkProvider} from '@clerk/clerk-react'
+
+//Import Your Publishable Key
+
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+
+if(!PUBLISHABLE_KEY)
+{
+  throw new Error("Missing Publishable Key")
+}
+createRoot(document.getElementById('root')).render(
+  
+  <ClerkProvider publishableKey = {PUBLISHABLE_KEY} afterSignOutUrl='/'>
+  <BrowserRouter 
+    future={{
+      v7_startTransition: true,
+      v7_relativeSplatPath: true,
+    }}>
+    
+      
+      <AppContextProvider>
+        <App />
+      </AppContextProvider>
+  </BrowserRouter>
+  </ClerkProvider>,
+)
---
+++ client/tailwind.config.js
@@ -0,0 +1,35 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontSize : 
+      {
+        'course-details-heading-small' : ['26px' , { lineHeight : '36px' }],
+        'course-details-heading-large' : ['36px' , { lineHeight : '44px' }],
+        'home-heading-small' : ['28px' , {lineHeight :  '34px' }],
+        'home-heading-large' : ['48px' , {lineHeight : '56px' }],
+        'default' : ['15px' , {lineHeight : '21px' }]
+      },
+      gridTemplateColumns : 
+      {
+        'auto' : 'repeat(auto-fit, minmax(200px, 250px))'
+      },
+      spacing :
+      {
+        'section-height' : '100vh'
+      },
+      maxWidth : {
+        'course-card' : '424px'
+      },
+      boxShadow : {
+        'custom-card' : '0px 4px 15px 2px rgba(0,0,0,0.1)',
+      }
+
+    },
+  },
+  plugins: [],
+}
